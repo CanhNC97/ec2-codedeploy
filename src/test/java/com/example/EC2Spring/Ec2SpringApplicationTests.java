d2053e653b571f1f2d202f6b42dda94914be219a
Nguyen <canhcong.nguyen@dxc.com>
2023-10-17 15:16:13 +0700

    Comment here

---
+++ src/test/java/com/example/EC2Spring/Ec2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EC2Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
